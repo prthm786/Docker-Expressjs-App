6e891ff86ae5f489132e4c023d93db896f69e13a
Pratham <100603618+prthm786@users.noreply.github.com>
2023-04-22 01:34:15 +0530

    Updating server.js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express")
+const {appendFileSync} = require("fs")
 const port = 5000
 
 const log = console.log
+
+# Creating express app 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+# Middleware 
+app.use((req, res, next) => {
+    appendFileSync("./logs.txt", `IP: ${req.id}`)
+    appendFileSync("./logs.txt", `Url: ${req.url}`)
+    appendFileSync("./logs.txt", `Method: ${req.method}`)
+
 })
 
+# Get request handler 
+app.get("/", (req, res) => {
+    res.send("Welcome to myNodejsApp")
+})
+
+# Handles Errors 
+app.get("*", (req, res) => {
+    res.send("Error 404 : Page Not Found")
+})
 
 app.listen(port, () => {
     log(`Server listening on ${port}`)
